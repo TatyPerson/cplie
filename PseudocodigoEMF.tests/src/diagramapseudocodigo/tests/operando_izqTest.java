3e791f5be53f6f0bb312f239feb31b5bf9d98d6c
tatyperson22@gmail.com <tatyperson22@gmail.com@7946f061-b1ed-ebed-2ed6-f3ea18cda720>
2014-02-11 12:05:52 +0000

    Ficheros "nuevos".

---
+++ PseudocodigoEMF.tests/src/diagramapseudocodigo/tests/operando_izqTest.java
@@ -0,0 +1,89 @@
+/**
+ */
+package diagramapseudocodigo.tests;
+
+import diagramapseudocodigo.DiagramapseudocodigoFactory;
+import diagramapseudocodigo.operando_izq;
+
+import junit.framework.TestCase;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>operando izq</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class operando_izqTest extends TestCase {
+
+	/**
+	 * The fixture for this operando izq test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected operando_izq fixture = null;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(operando_izqTest.class);
+	}
+
+	/**
+	 * Constructs a new operando izq test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public operando_izqTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Sets the fixture for this operando izq test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected void setFixture(operando_izq fixture) {
+		this.fixture = fixture;
+	}
+
+	/**
+	 * Returns the fixture for this operando izq test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected operando_izq getFixture() {
+		return fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(DiagramapseudocodigoFactory.eINSTANCE.createoperando_izq());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //operando_izqTest
